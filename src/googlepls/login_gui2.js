71dd4432bfb1e9b02bb2fb72c6acacc1d4ecc0d7
LiveWithMiracle <xin.steven926@hotmail.com>
2013-12-04 22:15:54 -0500

    get most of the twitter and google+ done

---
+++ src/googlepls/login_gui2.js
@@ -0,0 +1,9 @@
+ $(document).ready(function () {
+ $('#googlepls').click(function (e) {
+ alert('redirecting to Google+ signin page...');
+
+ var gplsLink = 'https://plus.google.com/?'
+ window.open(gplsLink,'_blank');
+ }
+ });
+ });
